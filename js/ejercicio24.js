0448fb298bf17d13daf172886d961ad4ab041376
Micaela-YSoria <soriayesica.cst@gmail.com>
2026-05-03 20:26:36 -0300

    Modifico archivos iniciales y creo el archivo de funciones.js

---
+++ js/ejercicio24.js
@@ -0,0 +1,10 @@
+import { actualizarSeleccion, mostrarEnConsola } from "./funciones24.js";
+
+const radios = document.querySelectorAll('input[name="lenguaje"]');
+
+radios.forEach(radio => {
+  radio.addEventListener("change", function () {
+    actualizarSeleccion(this.value);
+    mostrarEnConsola(this.value);
+  });
+});
---
+++ js/funciones24.js
@@ -0,0 +1,8 @@
+export function actualizarSeleccion(valor) {
+  const seleccion = document.getElementById("seleccion");
+  seleccion.textContent = "Seleccionado: " + valor;
+}
+
+export function mostrarEnConsola(valor) {
+  console.log("Seleccionado:", valor);
+}
