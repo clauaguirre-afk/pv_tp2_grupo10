b54e5db9a65011cc8cdc237d20ee11981200bf9a
Micaela-YSoria <soriayesica.cst@gmail.com>
2026-05-03 20:32:28 -0300

    Mejoré la indentación de los archivos del ejercicio 24

--- js/ejercicio24.js
+++ js/ejercicio24.js
@@ -3,8 +3,8 @@ import { actualizarSeleccion, mostrarEnConsola } from "./funciones24.js";
 const radios = document.querySelectorAll('input[name="lenguaje"]');
 
 radios.forEach(radio => {
-  radio.addEventListener("change", function () {
-    actualizarSeleccion(this.value);
-    mostrarEnConsola(this.value);
-  });
+    radio.addEventListener("change", function () {
+        actualizarSeleccion(this.value);
+        mostrarEnConsola(this.value);
+    });
 });
